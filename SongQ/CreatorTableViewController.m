5ad804764539a49ede858c2c11a1007265ab22ae
aclaussen1 <aclaussen@ymail.com>
2015-09-12 16:12:52 -0400

    Displays correct song to play next, Next step for this app is to actually get it to play the right song

--- SongQ/CreatorTableViewController.m
+++ SongQ/CreatorTableViewController.m
@@ -84,9 +84,14 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 3;
+
+   [self.partyUploaded fetch];
+    if([[self.partyUploaded objectForKey:@"votes"] count] ==  0){
+        return 2;
+    } else {
+        return 3;
+    }
+
 }
 
 
@@ -146,9 +151,15 @@
     }
     //shouldn't return null
     else {
+        
+        //[self.musicPlayer setQueueWithItemCollection:[MPMediaItemCollection collectionWithItems:[songsQuery items]]];
+        self.mediaItemCollection = [MPMediaItemCollection collectionWithItems:[[MPMediaQuery songsQuery] items]];
         WhatPeopleWantToHearTableViewCell * cell =[tableView dequeueReusableCellWithIdentifier:@"whatPeopleWantToHear" forIndexPath:indexPath];
         
         //http://stackoverflow.com/questions/12226776/finding-most-repeated-object-in-array
+        
+        [self.partyUploaded fetch];
+        
         //create count set from array
         NSCountedSet *setOfObjects = [[NSCountedSet alloc] initWithArray:[self.partyUploaded objectForKey:@"votes"]];
         
@@ -179,53 +190,14 @@
         } else {
             NSLog(@"artwork nil");
         }
+        
+        cell.title.text = [[self.mediaItemCollection.items objectAtIndex:mostOccuringObjectNSInteger] title];
+        cell.artist.text = [[self.mediaItemCollection.items objectAtIndex:mostOccuringObjectNSInteger] artist];
+        cell.album.text = [[self.mediaItemCollection.items objectAtIndex:mostOccuringObjectNSInteger] albumTitle];
+        cell.votes.text = [NSString stringWithFormat:@"Votes: %ld", (long)mostOccuringObjectNSInteger ];
         return cell;
     };
 }
 
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
